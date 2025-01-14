e1b857ec517e2ac65a0cec3c1226c39b2e9419bd
Aleksandra <aleksandragrzelak26@gmail.com>
2025-01-14 22:28:18 +0100

    page account added

---
+++ src/app/account/page.js
@@ -0,0 +1,70 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { auth, db } from "../firebase";
+import { doc, getDoc } from "firebase/firestore";
+import styles from "./Account.module.css";
+
+export default function Account() {
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  const navigateTo = (path) => () => router.push(path);
+
+  useEffect(() => {
+    const user = auth.currentUser;
+
+    if (!user) {
+      router.push("/login");
+      return;
+    }
+
+    const getUserData = async () => {
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setUserData(docSnap.data());
+      } else {
+        console.log("Brak danych użytkownika w Firestore");
+      }
+      setLoading(false);
+    };
+
+    getUserData();
+  }, [router]);
+
+
+  return (
+    <div className={styles.accountContainer}>
+      <header className={styles.header}>
+        <div className={styles.logo}>HankMed</div>
+      </header>
+
+      <div className={styles.mainContent}>
+        <h2 className={styles.h2Style}>Moje konto</h2>
+        <div className={styles.userInfo}>
+          {userData ? (
+            <>
+              <p><strong>Imię:</strong> {userData.name}</p>
+              <p><strong>Nazwisko:</strong> {userData.surname}</p>
+              <p><strong>PESEL:</strong> {userData.pesel}</p>
+            </>
+          ) : (
+            <p>Brak danych użytkownika.</p>
+          )}
+        </div>
+        <div className={styles.buttonContainer}>
+          <button className={styles.viewHistoryButton} onClick={navigateTo('/testResults')}>
+            Historia badań
+          </button>
+          <button className={styles.returnHomeButton} onClick={navigateTo('/')}>
+            Powrót do strony głównej
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
