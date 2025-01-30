f43ff276eefed40e04df19ed1474bb9e39cfd25e
Aleksandra <aleksandragrzelak26@gmail.com>
2025-01-30 18:57:01 +0100

    quick fix

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -51,9 +51,9 @@ export default function Account() {
               <p><strong>Imię:</strong> {userData.name}</p>
               <p><strong>Nazwisko:</strong> {userData.surname}</p>
               <p><strong>PESEL:</strong> {userData.pesel}</p>
-              <p><strong>Alergie:</strong> {userData.allergies}</p>
-              <p><strong>Leki:</strong> {userData.medications}</p>
-              <p><strong>Choroby przewlekłe:</strong> {userData.chronicDiseases}</p>
+              <p><strong>Alergie:</strong> {userData.allergiesInfo}</p>
+              <p><strong>Leki:</strong> {userData.medicationsInfo}</p>
+              <p><strong>Choroby przewlekłe:</strong> {userData.diseasesInfo}</p>
             </>
           ) : (
             <p>Brak danych użytkownika.</p>
